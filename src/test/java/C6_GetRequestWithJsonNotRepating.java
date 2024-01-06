3f48ed6e7f9536447d1c4477fd167e23fce34eb6
Recep ODEMİS <recepodemis6@gmail.com>
2024-01-06 13:35:06 +0300

    Uploaded C6 and C7 classes and Matchers hasItem, hasSize using example

---
+++ src/test/java/C6_GetRequestWithJsonNotRepating.java
@@ -0,0 +1,41 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class C6_GetRequestWithJsonNotRepating {
+
+    @Test
+    public void get01(){
+        /*
+        https://restful-booker.herokuapp.com/booking/11 url’ine bir GET request
+        gonderdigimizde donen Response’un,
+        status code’unun 200,
+        ve content type’inin application-json, ve response body’sindeki
+        "firstname“in, "Susan",
+        ve "lastname“in, "Jackson",
+        ve "totalprice“in, 612,
+        ve "depositpaid“in, false,
+        ve "additionalneeds“in, "Breakfast"
+        oldugunu test edin
+         */
+
+        String url = "https://restful-booker.herokuapp.com/booking/11";
+
+        Response response = given().when().get(url);
+
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("firstname",equalTo("John"),
+                        "lastname",equalTo("Smith"),
+                        "totalprice",equalTo(111),
+                        "depositpaid",equalTo(true),
+                        "additionalneeds",equalTo("Breakfast"));
+
+
+
+    }
+}
---
+++ src/test/java/C7_PutRequestWithJsonAutomationTest.java
@@ -0,0 +1,51 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class C7_PutRequestWithJsonAutomationTest {
+    @Test
+    public void putTest(){
+         /*
+            http://dummy.restapiexample.com/api/v1/employees sent get request
+                and check return Response
+                    status code : 200,
+                    content type : Application.JSON,
+                    In response body
+                    employees count : 24
+                    one of employee's is "Ashton Cox"
+                    include these employees ages 61,21 ve 35
+
+                     HERE IS RESULT OF GET REQUEST
+                    {
+                        "status": "success",
+                        "data": [
+                            {
+                                "id": 1,
+                                "employee_name": "Tiger Nixon",
+                                "employee_salary": 320800,
+                                "employee_age": 61,
+                                "profile_image": ""
+                            },
+                          .......
+                        ],
+                     }
+         */
+
+        String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+        Response response = given().when().get(url);
+
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON )
+                .body("data.id", hasSize(24),
+                        "data.employee_name", hasItem("Ashton Cox"),
+                        "data.employee_age", hasItems(61,21,35));
+
+
+    }
+}
